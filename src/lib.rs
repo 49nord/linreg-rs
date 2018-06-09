27dcaa0a28fe8d63a8f2edc851297914649924f4
Marc Brinkmann <marc@49nord.de>
2018-06-09 16:45:42 +0200

    Added more docs and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
 //! Linear regression
 //!
 //! `linreg` calculates linear regressions for two dimensional measurements.
-
+//!
+//! ```rust
+//!    use linreg::{linear_regression, linear_regression_of};
+//!
+//!    // Example 1: x and y values stored in two different vectors
+//!    let xs: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+//!    let ys: Vec<f64> = vec![2.0, 4.0, 5.0, 4.0, 5.0];
+//!
+//!    assert_eq!(Some((0.6, 2.2)), linear_regression(&xs, &ys));
+//!
+//!
+//!    // Example 2: x and y values stored as tuples
+//!    let tuples: Vec<(f32, f32)> = vec![(1.0, 2.0),
+//!                                       (2.0, 4.0),
+//!                                       (3.0, 5.0),
+//!                                       (4.0, 4.0),
+//!                                       (5.0, 5.0)];
+//!
+//!    assert_eq!(Some((0.6, 2.2)), linear_regression_of(&tuples));
+//!
+//!
+//!    // Example 3: directly operating on integer (will be convert to float as needed)
+//!    let xs: Vec<u8> = vec![1, 2, 3, 4, 5];
+//!    let ys: Vec<u8> = vec![2, 4, 5, 4, 5];
+//!
+//!    assert_eq!(Some((0.6, 2.2)), linear_regression(&xs, &ys));
+//! ```
 #![no_std]
 
 extern crate num_traits;
@@ -18,6 +44,9 @@ use std::vec::Vec;
 use core::iter::Iterator;
 
 pub trait IteratorMean<F> {
+    /// Calculates the mean value of all returned items of an iterator. Returns
+    /// `None` if either no items are present or more items than can be counted
+    /// by `F` (conversion from `usize` to  `F` is not possible).
     fn mean(&mut self) -> Option<F>;
 }
 
@@ -66,7 +95,17 @@ fn empty_set_has_no_mean() {
     assert!(res.is_none());
 }
 
-fn lin_reg<'a, X, Y, IX, IY, F>(xs: IX, ys: IY, x_mean: F, y_mean: F) -> Option<(F, F)>
+/// Calculates a linear regression
+///
+/// Lower-level linear regression function. Assumes that `x_mean` and `y_mean`
+/// have already been calculated. Returns `None` if
+///
+/// * the slope is too steep to represent, approaching infinity
+///
+/// Since there is a mean, this function assumes that `xs` and `ys` are both non-empty.
+///
+/// Returns `Some(slope, intercept)` of the regression line.
+pub fn lin_reg<'a, X, Y, IX, IY, F>(xs: IX, ys: IY, x_mean: F, y_mean: F) -> Option<(F, F)>
 where
     X: 'a + Into<F> + Clone,
     Y: 'a + Into<F> + Clone,
@@ -101,6 +140,16 @@ where
 }
 
 /// Linear regression
+///
+/// Calculates the linear regression from two slices, one for x- and one for y-values.
+///
+/// Returns `None` if
+///
+/// * `xs` and `ys` differ in length
+/// * `xs` or `ys` do not have a mean (e.g. if they are empty, see `IteratorMean` for details)
+/// * the slope is too steep to represent, approaching infinity
+///
+/// Returns `Some(slope, intercept)` of the regression line.
 pub fn linear_regression<X, Y, F>(xs: &[X], ys: &[Y]) -> Option<(F, F)>
 where
     X: Clone + Into<F>,
@@ -118,14 +167,24 @@ where
     lin_reg(xs.iter(), ys.iter(), x_mean, y_mean)
 }
 
-pub fn linear_regression_on<X, Y, F>(xys: &[(X, Y)]) -> Option<(F, F)>
+/// Linear regression
+///
+/// Calculates the linear regression from a slice of tuple values.
+///
+/// Returns `None` if
+///
+/// * `x` or `y` tuple members do not have a mean (e.g. if they are empty, see `IteratorMean` for details)
+/// * the slope is too steep to represent, approaching infinity
+///
+/// Returns `Some(slope, intercept)` of the regression line.
+pub fn linear_regression_of<X, Y, F>(xys: &[(X, Y)]) -> Option<(F, F)>
 where
     X: Clone + Into<F>,
     Y: Clone + Into<F>,
     F: Float,
 {
-    // FIXME: cache penalty here, we should be calculating both means in a single step to avoid
-    //        iterating twice
+    // FIXME: cache penalty here, we should be calculating both means in a single step
+    //        to avoid iterating twice
     let x_mean = xys.iter().map(|(x, _)| x).mean()?;
     let y_mean = xys.iter().map(|(_, y)| y).mean()?;
 
@@ -143,6 +202,13 @@ fn test_example_regression() {
     let ys: Vec<f64> = vec![2.0, 4.0, 5.0, 4.0, 5.0];
 
     assert_eq!(Some((0.6, 2.2)), linear_regression(&xs, &ys));
+}
+
+#[test]
+fn test_example_regression_of() {
+    let tuples: Vec<(f32, f32)> = vec![(1.0, 2.0), (2.0, 4.0), (3.0, 5.0), (4.0, 4.0), (5.0, 5.0)];
+
+    assert_eq!(Some((0.6, 2.2)), linear_regression_of(&tuples));
 }
 
 #[test]
